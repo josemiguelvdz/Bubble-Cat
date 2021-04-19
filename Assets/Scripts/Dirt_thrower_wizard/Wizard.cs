87f91ac9d62de628bb9069d4607627911e021225
jocarr01 <82376126+jocarr01@users.noreply.github.com>
2021-04-19 20:42:25 +0200

    Se modifica los placeHolders del lagarto
    Se realiza modificaciones en el placeHolder del lagarto, se plantea la opción de que deje de disparar cuando entra en la pompa. Esta pendiente el placeHolder del proyectil.

--- Assets/Scripts/Dirt_thrower_wizard/Wizard.cs
+++ Assets/Scripts/Dirt_thrower_wizard/Wizard.cs
@@ -11,13 +11,14 @@ public class Wizard : MonoBehaviour
     //Indica el tiempo que pasa entre un proyectil y el siguiente
     public float shootCadenceSecs;
     //Indica si queremos que haya una repeticion de projectiles o no
-    public bool autoShoot;
-    //Se indica el tiempo de retardo tras el primer invoke
-    private float initialTime;  
+    public bool autoShoot = true;
     //Indica si el lagarto puede disparar
-    private bool canShoot = true;
+    public bool canShoot = true;
+    //Se indica el tiempo de retardo tras el primer invoke
+    private float initialTime;      
     //Indica la direction en la que se mueve el lagarto, empezamos con movimiento hacia la derecha
     string direction = "right";
+    Vector3 posSpawner = new Vector3(0.5f, -0.5f, 0);
 
 
     void Start()
@@ -25,6 +26,7 @@ public class Wizard : MonoBehaviour
         initialTime = shootCadenceSecs;
         //Asignamos la posicion del spawner en una poscion en concreto en base a la poscion del lagarto
         spawnerProjectile.transform.position = transform.position + new Vector3(0.5f, -0.5f, 0);
+        dirtProjectile.transform.position = spawnerProjectile.transform.position + new Vector3(0, -0.15f, 0);
 
         if (autoShoot) //Esta activado el autoShoot se generan continuamente proyectiles
             InvokeRepeating("Shoot", initialTime, shootCadenceSecs);
@@ -36,24 +38,25 @@ public class Wizard : MonoBehaviour
     {
         //Obtenemos el nuevo escale por si haya cambiado y haya que rotar al lagarto
         if (direction == "right")
-        {            
-            //Indicamos la traslacion (movimeinto) del lagarto hacia la derecha
+        {
+            //Indicamos la traslacion (movimiento) del lagarto hacia la derecha
             transform.Translate(Vector3.right * Time.deltaTime);
+            spawnerProjectile.transform.position = transform.position + posSpawner;
         }
         if (direction == "left")
         {
-            //Indicamos la traslacion (movimeinto) del lagarto hacia la izquierda
+            //Indicamos la traslacion (movimiento) del lagarto hacia la izquierda
             transform.Translate(Vector3.left * Time.deltaTime);
+            spawnerProjectile.transform.position = new Vector3((transform.position.x + -posSpawner.x), (transform.position.y + posSpawner.y), (transform.position.z + posSpawner.z));
         }
         //Actualizamos la posicion tanto del spawner como del proyectil ya que el lagarto esta en movimiento continuo, y al instanciar el nuevo proyectil debe tener estas referencias
-        spawnerProjectile.transform.position = transform.position + new Vector3(0.5f, -0.5f, 0);
-        dirtProjectile.transform.position = spawnerProjectile.position + new Vector3(0, 1.25f, 0);
+        dirtProjectile.transform.position = spawnerProjectile.position + new Vector3(0, -0.15f, 0);
     }
 
     //Dispara bala en la direction dada
     public void Shoot()
     {
-        if (canShoot)
+        if (canShoot == true)
         {
             Instantiate(dirtProjectile, dirtProjectile.transform.position, Quaternion.Euler(new Vector3(0, 0, 105)));
         }
@@ -68,7 +71,7 @@ public class Wizard : MonoBehaviour
         if (collision.gameObject.layer == stageLayer)
         {
             //Como hemos colisionado con un stage hay que rotar al lagarto para que el movimiento sea acorde a la direccion
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);            
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             //La direccion del movimiento es derecha, asi que cambia la direccion del movimiento hacia la izquierda
             if (direction == "right") 
             {
@@ -79,5 +82,12 @@ public class Wizard : MonoBehaviour
                 direction = "right";
             }
         }
+    }
+
+    public void stopShoothing()
+    {
+        canShoot = false;
+        Debug.Log("Parar de disparar");
+        return;
     }
 }
