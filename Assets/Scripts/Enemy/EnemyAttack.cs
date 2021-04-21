36177b6a5b8790ebd97bb8148b5ab24aaa5f29cc
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-04-21 10:01:31 +0200

    slime
    Añadido enemigo slime con sus scripts

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    EnemyMovement stop;
+
+    void Start()
+    {
+        stop = GetComponentInParent<EnemyMovement>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            Debug.Log("Ataco");
+            stop.direction = Vector2.zero;
+        }
+    }
+}
