40b5582482e29d4f3d8b43f5b2147e461cc3ae92
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2021-05-03 13:33:23 +0200

    Creado las tuberías y arreglo de un par de cosas
    He añadido el prefab tuberia rota, y el prefab tuberia, los scripts necesarios para que la tuberia rota se arregle al colocar una tuberia, tambien le he puesto la imagen de butano al prefab, que no lo tenia, y he creado un script ConvertToDinamic para que los objetos al principio sean staticos pero cuando los toques se conviertan en dinamicos

---
+++ Assets/Scripts/ConvertToDinamic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToDinamic : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/RestorePipeline.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestorePipeline : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Pipeline>())
+        {
+            Destroy(collision.gameObject);
+            transform.GetChild(0).gameObject.SetActive(true);
+            GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(this);
+        }
+    }
+}
