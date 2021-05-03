2b23dcd9c83e283cdb2f34ed0b5b3b6a53b5eee8
jocarr01 <82376126+jocarr01@users.noreply.github.com>
2021-05-03 21:38:11 +0200

    Primeros avances MagicRay

--- Assets/Scripts/Bastet/Magic/Magic.cs
+++ Assets/Scripts/Bastet/Magic/Magic.cs
@@ -2,15 +2,17 @@
 
 public class Magic : MonoBehaviour
 {
+    public Transform spawner;
+    public GameObject magicRay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("InvokeRay", 3, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Instantiate(magicRay, spawner.position, Quaternion.Euler(new Vector3(0, 0, 180)));
     }
 }
---
+++ Assets/Scripts/Bastet/Magic/MagicRay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicRay : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = Vector2.up * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.GetComponent<BubbleController>())
+            Destroy(this.gameObject);
+    }
+}
