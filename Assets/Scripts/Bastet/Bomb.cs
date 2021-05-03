f118e32e7ce059b32a581c27b6d18226c371fbf7
javics2002 <49459590+javics2002@users.noreply.github.com>
2021-05-03 19:19:52 +0200

    Shoot y Bomb
    Hechos los scripts de Shoot y Bomb pero no los puedo probar porque me faltan unos prefabs!!

--- Assets/Scripts/Bastet/Bomb.cs
+++ Assets/Scripts/Bastet/Bomb.cs
@@ -2,15 +2,25 @@
 
 public class Bomb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("Prefab de la bomba de butano a instanciar en el Spawn Point"), SerializeField]
+    GameObject bomb;
+
+    [Tooltip("Aquí se instancia la bomba de butano"), SerializeField]
+    Transform spawnPoint;
+
+    [Tooltip("Dirección que toma la bomba de butano"), SerializeField]
+    Vector3 direction;
+
+    [Tooltip("Tiempo que tarda Bastet en cargar la bomba de butano y disparar"), SerializeField]
+    float chargeTime;
+
+    private void OnEnable()
     {
-        
+        Invoke("Fire", chargeTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Fire()
     {
-        
+        Instantiate(bomb, spawnPoint);
     }
 }
