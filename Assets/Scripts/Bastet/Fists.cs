9cbf20b9e753977a83fe27757dffcc871ff3524b
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-05-03 20:00:58 +0200

    FistBastet
    Añadida una primera versión del ataque de puños

--- Assets/Scripts/Bastet/Fists.cs
+++ Assets/Scripts/Bastet/Fists.cs
@@ -2,15 +2,45 @@
 
 public class Fists : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject fist;
+    public float start, cooldown;
+    int spawn, firstSon, secondSon;
+
     void Start()
     {
-        
+        InvokeRepeating("InvokeFit", start, cooldown);
     }
 
-    // Update is called once per frame
-    void Update()
+    void InvokeFit()
     {
-        
+        spawn = Random.Range(0, 2);
+
+        if (spawn == 0)
+        {
+            firstSon++;
+        }
+
+        else
+        {
+            secondSon++;
+        }
+
+        if (firstSon == 3)
+        {
+            spawn = 1;
+            secondSon = 1;
+            firstSon = 0;
+        }
+
+        else if (secondSon == 3)
+        {
+            spawn = 0;
+            firstSon = 1;
+            secondSon = 0;
+        }
+
+        Debug.Log(spawn);
+
+        Instantiate(fist, transform.GetChild(spawn).position, fist.transform.rotation);
     }
 }
