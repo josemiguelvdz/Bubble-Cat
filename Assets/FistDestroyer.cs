586d350b64b1ac9eaa9b4c32c622ac1613fba120
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-05-04 13:57:22 +0200

    FistBastet
    Añadida la mecanica de puños de bastet

---
+++ Assets/FistDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistDestroyer : MonoBehaviour
+{
+    public float timeDestroyer;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, timeDestroyer);
+    }
+}
