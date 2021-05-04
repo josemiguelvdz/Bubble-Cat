b68c2f498343901215cf3f4313eb5a72bcc7f568
jocarr01 <82376126+jocarr01@users.noreply.github.com>
2021-05-04 10:52:28 +0200

    Ajustes Magic

--- Assets/Scripts/Bastet/Magic/Magic.cs
+++ Assets/Scripts/Bastet/Magic/Magic.cs
@@ -4,15 +4,37 @@ public class Magic : MonoBehaviour
 {
     public Transform spawner;
     public GameObject magicRay;
+    public float rateTime;
+    public float cooldownTime;
+    private GameObject player; //Posicion del jugador
+    Vector3 directionMagicRay;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("InvokeRay", 3, 5);
+        player = GameManager.GetInstance().GetPlayer();
+        InvokeRepeating("InvokeRay", rateTime, cooldownTime);
     }
 
     // Update is called once per frame
     void Update()
+    {        
+    }
+
+    void InvokeRay()
+    {        
+        setDirection(player.transform.position - spawner.position);
+        float angle = Vector3.Angle(getDirection(), transform.forward); //Angulo que indica la direccion del rayo para rotar el transform del rayo magico
+        Instantiate(magicRay, spawner.position, Quaternion.Euler(new Vector3(0, 0, angle)));
+    }
+
+    public void setDirection(Vector3 vector)
     {
-        Instantiate(magicRay, spawner.position, Quaternion.Euler(new Vector3(0, 0, 180)));
+        directionMagicRay = vector;
+    }
+
+    public Vector3 getDirection()
+    {
+        return directionMagicRay;
     }
 }
--- Assets/Scripts/Bastet/Magic/MagicRay.cs
+++ Assets/Scripts/Bastet/Magic/MagicRay.cs
@@ -6,16 +6,21 @@ public class MagicRay : MonoBehaviour
 {
     public float speed;
     Rigidbody2D rb;
+    Magic parent;
+    Vector3 direction;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        parent = FindObjectOfType<Magic>();
+        direction = parent.getDirection();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = Vector2.up * speed * Time.fixedDeltaTime;
+        //rb.velocity = Vector2.left * speed * Time.fixedDeltaTime;
+        rb.velocity = direction.normalized * speed * Time.fixedDeltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
