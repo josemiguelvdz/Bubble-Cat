496f45a62d286e0bbf146d20a9461e3d894d0b3e
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-05-05 01:21:41 +0200

    Añadida lanzatapas y cascada

---
+++ Assets/Scripts/ManHoleSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManHoleSpawner : MonoBehaviour
+{
+    public float cooldown, time;
+    public GameObject ManHole;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnManHole", time, cooldown);
+    }
+
+    void SpawnManHole()
+    {
+        Instantiate(ManHole, transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Slime/SlimeBehaviour.cs
+++ Assets/Scripts/Slime/SlimeBehaviour.cs
@@ -3,7 +3,7 @@
 public class SlimeBehaviour : MonoBehaviour
 {
     GameObject player;
-    RaycastHit2D hitPlayer, hitWall;
+    RaycastHit2D hitPlayer, hitWall, hitGround;
     Vector3 direction = Vector3.left;
     float distance;
     public float slimeSpeed, coolDown, wallDistance;
@@ -25,8 +25,17 @@ public class SlimeBehaviour : MonoBehaviour
             distance = Vector3.Distance(player.transform.position, transform.position);
         }
 
-        hitWall = Physics2D.Raycast(transform.position + direction, direction, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
-        Debug.DrawRay(transform.position + direction, direction, Color.red);
+        hitWall = Physics2D.Raycast(transform.position, direction, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+        hitGround = Physics2D.Raycast(transform.position, Vector2.down, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
+        Debug.DrawRay(transform.position, Vector2.down, Color.yellow);
+
+        if (hitGround.collider == null)
+        {
+            direction = -direction;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
 
         if ((distance > visionRadius) && hitWall.collider != null && (hitWall.collider.CompareTag("Ground") || hitWall.collider.GetComponent<Bubbleable>()))
         {
@@ -35,7 +44,7 @@ public class SlimeBehaviour : MonoBehaviour
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
 
-        if ((distance > visionRadius) || (distance > attackRadius && distance < visionRadius && hitWall.collider == null))
+        if ((distance > visionRadius) || (distance > attackRadius && distance < visionRadius && (hitWall.collider == null || hitGround.collider == null)))
         {
             transform.Translate(slimeSpeed * direction * Time.deltaTime);
             Debug.Log("Me muevo por la cara");
