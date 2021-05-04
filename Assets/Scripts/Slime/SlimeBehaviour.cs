5ef5a2ae95f2f16a38c7e1bd3872260d4f691d4f
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-05-05 01:13:57 +0200

    Revert "Añadidos Cascada Agua y LanzaTapas"
    This reverts commit 92acd40f457dbb26d2d9f131dfb93d0e50b59965.

--- Assets/Scripts/Slime/SlimeBehaviour.cs
+++ Assets/Scripts/Slime/SlimeBehaviour.cs
@@ -3,7 +3,7 @@
 public class SlimeBehaviour : MonoBehaviour
 {
     GameObject player;
-    RaycastHit2D hitPlayer, hitWall, hitGround;
+    RaycastHit2D hitPlayer, hitWall;
     Vector3 direction = Vector3.left;
     float distance;
     public float slimeSpeed, coolDown, wallDistance;
@@ -25,17 +25,8 @@ public class SlimeBehaviour : MonoBehaviour
             distance = Vector3.Distance(player.transform.position, transform.position);
         }
 
-        hitWall = Physics2D.Raycast(transform.position, direction, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
-        Debug.DrawRay(transform.position, direction, Color.red);
-
-        hitGround = Physics2D.Raycast(transform.position, Vector2.down, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
-        Debug.DrawRay(transform.position, Vector2.down, Color.yellow);
-
-        if (hitGround.collider == null)
-        {
-            direction = -direction;
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        }
+        hitWall = Physics2D.Raycast(transform.position + direction, direction, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
+        Debug.DrawRay(transform.position + direction, direction, Color.red);
 
         if ((distance > visionRadius) && hitWall.collider != null && (hitWall.collider.CompareTag("Ground") || hitWall.collider.GetComponent<Bubbleable>()))
         {
@@ -44,7 +35,7 @@ public class SlimeBehaviour : MonoBehaviour
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
 
-        if ((distance > visionRadius) || (distance > attackRadius && distance < visionRadius && (hitWall.collider == null || hitGround.collider == null)))
+        if ((distance > visionRadius) || (distance > attackRadius && distance < visionRadius && hitWall.collider == null))
         {
             transform.Translate(slimeSpeed * direction * Time.deltaTime);
             Debug.Log("Me muevo por la cara");
