aa2784c24a51ca1e300f583f6e7c42b7ad296d84
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-05-05 22:50:58 +0200

    Slime arreglado 100%%

--- Assets/Scripts/Slime/SlimeBehaviour.cs
+++ Assets/Scripts/Slime/SlimeBehaviour.cs
@@ -3,15 +3,17 @@
 public class SlimeBehaviour : MonoBehaviour
 {
     GameObject player;
+    Rigidbody2D rb;
     RaycastHit2D hitPlayer, hitWall, hitGround;
     Vector3 direction = Vector3.left;
     float distance;
-    public float slimeSpeed, coolDown, wallDistance;
-    public float attackRadius, visionRadius;
+    public float slimeSpeed, wallDistance,force;
+    public float visionRadius;
 
     void Start()
     {
         player = GameManager.GetInstance().GetPlayer();
+        rb = GetComponent<Rigidbody2D>();
         distance = 100f;
     }
 
@@ -26,10 +28,10 @@ public class SlimeBehaviour : MonoBehaviour
         }
 
         hitWall = Physics2D.Raycast(transform.position, direction, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
-        Debug.DrawRay(transform.position, direction, Color.red);
+        Debug.DrawRay(transform.position, direction.normalized*wallDistance, Color.red);
 
         hitGround = Physics2D.Raycast(transform.position, Vector2.down, wallDistance, 1 << LayerMask.NameToLayer("Stage"));
-        Debug.DrawRay(transform.position, Vector2.down, Color.yellow);
+        Debug.DrawRay(transform.position, Vector2.down.normalized*wallDistance, Color.yellow);
 
         if (hitGround.collider == null)
         {
@@ -37,20 +39,19 @@ public class SlimeBehaviour : MonoBehaviour
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
 
-        if ((distance > visionRadius) && hitWall.collider != null && (hitWall.collider.CompareTag("Ground") || hitWall.collider.GetComponent<Bubbleable>()))
+        if ((distance > visionRadius) && hitWall.collider != null && hitWall.collider.gameObject.layer == 9)
         {
             Debug.Log("Muro encontrado");
             direction = -direction;
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
 
-        if ((distance > visionRadius) || (distance > attackRadius && distance < visionRadius && (hitWall.collider == null || hitGround.collider == null)))
+        if ((distance > visionRadius) || (distance < visionRadius && (hitWall.collider == null && hitGround.collider != null)))
         {
             transform.Translate(slimeSpeed * direction * Time.deltaTime);
-            Debug.Log("Me muevo por la cara");
         }
 
-        if (distance < visionRadius && distance > attackRadius)
+        if (distance < visionRadius)
         {
             Debug.Log("jugador encontrado");
 
@@ -68,10 +69,17 @@ public class SlimeBehaviour : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            rb.AddForce(-direction*force, ForceMode2D.Impulse);
+        }
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, visionRadius);
-        Gizmos.DrawWireSphere(transform.position, attackRadius);
     }
 }
