3f051abbbfc12b36fcb67a625d3970de4e1368c0
jocarr01 <82376126+jocarr01@users.noreply.github.com>
2021-05-06 13:02:14 +0200

    Ajustes Lizard

--- Assets/Scripts/Dirt_thrower_wizard/Lizard.cs
+++ Assets/Scripts/Dirt_thrower_wizard/Lizard.cs
@@ -22,7 +22,7 @@ public class Lizard : MonoBehaviour
     //Se indica el tiempo de retardo tras el primer invoke
     private float initialTime;
     //Indica la direction en la que se mueve el lagarto, empezamos con movimiento hacia la derecha
-    private string direction = "right";
+    private string direction;
     //Indica la velocidad del descenso del lagarto
     private float dropVelocity = 5;
     //Indica la rotacion inicial, para perder dicha referencia al rotar al lagarto dentro de la pompa 
@@ -36,8 +36,8 @@ public class Lizard : MonoBehaviour
     private float radius; //Distancia a la que veo al jugador
 
     Vector3 dir;
-
     RaycastHit2D hitStage;
+    bool hitWall;
 
     void Start()
     {
@@ -46,13 +46,14 @@ public class Lizard : MonoBehaviour
         initialTime = shootCadenceSecs;
         initialTrans = transform.rotation;
         dir = Vector2.zero;
+        hitWall = false;
+        direction = "right";
     }
 
     private void Update()
     {
         if (String.IsNullOrEmpty(direction))
         {
-            transform.Translate(Vector3.zero * Time.deltaTime);
             killLizard();
         }
         else
@@ -60,66 +61,71 @@ public class Lizard : MonoBehaviour
             //Detectamos al jugador y trazamos un rayo hacia él
             if (player != null)
             {
-                //Debug.Log("Detecto player");
+                movimiento(direction);
                 Shoot();
             }
+        }
+    }
 
-            if (direction != "drop")//No tiene que caerse el lagarto
+    private void movimiento(string direction)
+    {
+        if (this.direction != "drop")//No tiene que caerse el lagarto
+        {
+            hitStage = Physics2D.Raycast(foot.position, Vector2.up, 0.5f);
+            Debug.DrawRay(foot.position, Vector2.up * 0.5f, Color.red);
+
+            if (hitStage.collider != null && (hitStage.collider.name == "Tilemap" || hitWall == false)) //Detecta suelo
             {
-                hitStage = Physics2D.Raycast(foot.position, Vector2.up, 0.5f);
-                //Debug.DrawRay(foot.position, Vector2.up * 0.5f, Color.green);
-
-                if (!hitStage && hitStage.collider != null) //Detecta suelo
+                if (this.direction == "right")
                 {
-                    if (direction == "right")
-                    {
-                        //Indicamos la traslacion (movimiento) del lagarto hacia la derecha y cambio del sprite (volteo)
-                        transform.Translate(Vector3.right * Time.deltaTime);
-                        sprite.flipX = false;
-                    }
-                    else if (direction == "left")
-                    {
-                        //Indicamos la traslacion (movimiento) del lagarto hacia la izquierda yu cambio del scripte (volteo)
-                        transform.Translate(Vector3.left * Time.deltaTime);
-                        sprite.flipX = true;
-                    }
+                    //Indicamos la traslacion (movimiento) del lagarto hacia la derecha y cambio del sprite (volteo)
+                    transform.Translate(Vector3.right * Time.deltaTime);
+                    sprite.flipX = false;
                 }
-                else //No detecta suelo
+                else if (this.direction == "left")
                 {
-                    //Cambiamos al direccion del lagarto y actualizamos la posicion del pie
-                    foot.localPosition = new Vector3(-foot.localPosition.x, foot.localPosition.y, foot.localPosition.z);
-                    if (direction == "right")
-                    {
-                        direction = "left";
-                        //foot.position = new Vector3(-foot.position.x, foot.position.y, foot.position.z);
-                    }
-                    else if (direction == "left")
-                    {
-                        direction = "right";
-                        //foot.position = new Vector3(foot.position.x, foot.position.y, foot.position.z);
-                    }
+                    //Indicamos la traslacion (movimiento) del lagarto hacia la izquierda yu cambio del scripte (volteo)
+                    transform.Translate(Vector3.left * Time.deltaTime);
+                    sprite.flipX = true;
                 }
             }
-            else//Tiene que caerse el lagarto
+            else if (hitStage.collider == null || hitStage.collider.name != "Tilemap") //No detecta suelo
             {
-                //Asignamos la rotacion al lagarto, para generar un movimineto de caida vertical, ya que al haberlo rotado el movimiento saldra con la rotacion de la manipulacion de la pompa.
-                transform.rotation = initialTrans;
-                //Indicamos la traslacion (movimiento) del lagarto en descenso
-                transform.Translate(Vector3.down * dropVelocity * Time.deltaTime);
+                if (this.direction == "right")
+                {
+                    this.direction = "left";
+                    //foot.position = new Vector3(-foot.position.x, foot.position.y, foot.position.z);
+                }
+                else if (this.direction == "left")
+                {
+                    this.direction = "right";
+                    //foot.position = new Vector3(foot.position.x, foot.position.y, foot.position.z);
+                }
+                hitWall = false;
+                foot.localPosition = new Vector3(-foot.localPosition.x, foot.localPosition.y, foot.localPosition.z);
             }
+        }
+        else//Tiene que caerse el lagarto
+        {
+            //Asignamos la rotacion al lagarto, para generar un movimineto de caida vertical, ya que al haberlo rotado el movimiento saldra con la rotacion de la manipulacion de la pompa.
+            transform.rotation = initialTrans;
+            //Indicamos la traslacion (movimiento) del lagarto en descenso
+            transform.Translate(Vector3.down * dropVelocity * Time.deltaTime);
         }
     }
 
     //Dispara bala en la direction dada
     public void Shoot()
     {
-        Debug.Log("Intento disparar");
         if (Time.time > initialTime && canShoot == true)
         {
-            Debug.Log("Disparo");
-            initialTime = Time.time + shootCadenceSecs;
-            Instantiate(dirtProjectile, spawnerProjectile.position, Quaternion.Euler(new Vector3(0, 0, 0)));
-            setDirectionDirtProjectile(player.position - spawnerProjectile.position);
+            initialTime = Time.time + shootCadenceSecs;         
+            Vector3 distance = player.position - spawnerProjectile.position;
+            if (Mathf.Abs(distance.x) >= 0 && Mathf.Abs(distance.x) <= radius)
+            {
+                Instantiate(dirtProjectile, spawnerProjectile.position, Quaternion.Euler(new Vector3(0, 0, 90)));
+                setDirectionDirtProjectile(distance);
+            }
         }
     }
 
