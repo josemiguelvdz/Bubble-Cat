f9e8c36dea662a0e76a94d253f7d10e8b2029901
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2021-05-06 20:49:24 +0200

    Araña perfecta
    Solucionado todo lo de la araña

--- Assets/Scripts/Spider/SpiderMovement.cs
+++ Assets/Scripts/Spider/SpiderMovement.cs
@@ -3,7 +3,7 @@
 public class SpiderMovement : MonoBehaviour
 {
     public float spiderSpeed,escapeSpeed,visionRadius,attackRadius;
-    public float timeToAttack, forceX, forceY, refreshAttack, raycastLength;
+    public float timeToAttack, forceX, forceY, refreshAttack, raycastLength,wallDistance;
     public LayerMask stageLayer;
 
 
@@ -13,6 +13,7 @@ public class SpiderMovement : MonoBehaviour
     GameObject player;
     Rigidbody2D rb;
     Quaternion playerRotation;
+    RaycastHit2D wall;
 
     private void Start()
     {
@@ -30,6 +31,32 @@ public class SpiderMovement : MonoBehaviour
             if (player.transform.position.x - transform.position.x < 0 ) transform.localScale = new Vector3(izq, transform.localScale.y, transform.localScale.z);
             else if (player.transform.position.x - transform.position.x > 0 ) transform.localScale = new Vector3(der, transform.localScale.y, transform.localScale.z);
         }
+        else
+        {
+            wall = Physics2D.Raycast(transform.position,new Vector2(transform.localScale.x, 0).normalized, wallDistance, stageLayer);
+            Debug.DrawRay(transform.position,new Vector2(transform.localScale.x, 0), Color.red);
+
+            if(wall.collider!=null)Debug.Log(wall.collider.name);
+
+            if (wall.collider != null)
+            {
+                if (transform.localScale.x <= 0 )
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                    Debug.Log("Derecha");
+                }
+                else if (transform.localScale.x > 0)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                    Debug.Log("Izquierda");
+                }
+            }
+            else
+            {
+                rb.velocity = new Vector2(escapeSpeed * transform.localScale.x, 0);
+            }
+        }
+
     }
 
 
@@ -40,6 +67,8 @@ public class SpiderMovement : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, visionRadius, 1 << LayerMask.NameToLayer("Player"));
         Vector3 forward = transform.TransformDirection(player.transform.position - transform.position);
         Debug.DrawRay(transform.position, forward, Color.red);
+
+
 
 
         if (Physics2D.Raycast(transform.position, Vector2.down, raycastLength, stageLayer))
@@ -60,25 +89,28 @@ public class SpiderMovement : MonoBehaviour
             distance = Vector3.Distance(player.transform.position, transform.position);
         }
 
-        
-        if (distance < visionRadius && distance > attackRadius && ground)
+        if (distance < attackRadius && !attacking && ground)
+        {
+            rb.velocity = Vector3.zero;
+            attacking = true;
+            Invoke("Attack", timeToAttack);
+        }
+        else if (distance < visionRadius && distance > attackRadius && ground && !attacking) 
         {
             rb.velocity = new Vector2(player.transform.position.x - transform.position.x, rb.velocity.y).normalized * spiderSpeed;
             if (!attacking) CancelInvoke();
             else attacking = false;
             runnningAway = false;
         }
-        else if (distance < attackRadius && !attacking && ground)
+
+        else if(distance > visionRadius && ground && !attacking)
         {
-            rb.velocity = Vector3.zero;
-            attacking = true;
-            Invoke("Attack", timeToAttack);
-        }
-        else if(distance > visionRadius && ground)
-        {
+            if (!runnningAway)
+            {
+                if (player.transform.position.x < transform.position.x) transform.localScale = new Vector3(der, transform.localScale.y, transform.localScale.z);
+                else transform.localScale = new Vector3(izq, transform.localScale.y, transform.localScale.z);
+            }
             runnningAway = true;
-            rb.velocity = new Vector2(-player.transform.position.x + transform.position.x, rb.velocity.y).normalized * escapeSpeed;
-            transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z);
         }
     }
 
@@ -91,13 +123,16 @@ public class SpiderMovement : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, attackRadius);
     }
 
+
+
+
     void Attack()
     {
-        if(transform.localScale.x<0) rb.AddForce(new Vector2(-forceX, forceY), ForceMode2D.Impulse);
-        else if(transform.localScale.x>0) rb.AddForce(new Vector2(forceX, forceY), ForceMode2D.Impulse);
-
+        if (transform.localScale.x < 0) rb.AddForce(new Vector2(-forceX, forceY), ForceMode2D.Impulse);
+        else if (transform.localScale.x > 0) rb.AddForce(new Vector2(forceX, forceY), ForceMode2D.Impulse);
         Invoke("CanAttack", refreshAttack);
     }
+
 
     void CanAttack()
     {
