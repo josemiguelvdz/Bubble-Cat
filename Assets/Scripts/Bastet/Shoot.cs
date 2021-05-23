f232f76c2db8797fcecba5ec2f1cb3257decd0a5
josemiguelvdz <josemiguelv2002@gmail.com>
2021-05-23 12:56:52 +0200

    Progreso Bastet

--- Assets/Scripts/Bastet/Shoot.cs
+++ Assets/Scripts/Bastet/Shoot.cs
@@ -23,6 +23,13 @@ public class Shoot : MonoBehaviour
     [Tooltip("Probabilidad de que el siguiente ataque sean los puños"), Range(0, 100f), SerializeField]
     float prob = 40f;
 
+    [Tooltip("Cañon 1"), SerializeField]
+    Animator cannon1Anim = null;
+    [Tooltip("Cañon 2"), SerializeField]
+    Animator cannon2Anim = null;
+    [Tooltip("Aluminio protector"), SerializeField]
+    Animator alumAnim = null;
+
     int proyectilesLeft;
     Transform player;
     Bastet bastet;
@@ -37,6 +44,7 @@ public class Shoot : MonoBehaviour
         proyectilesLeft = ammo;
 
         InvokeRepeating("Burst", chargeTime, shootRate);
+
     }
 
     private void Start()
@@ -53,11 +61,22 @@ public class Shoot : MonoBehaviour
     {
         GameObject bag = Instantiate(proyectile, spawnPoint.position, Quaternion.identity);
         bag.GetComponent<Rigidbody2D>().AddForce(impulse * Random.Range(0.5f, 2f), ForceMode2D.Impulse);
+
+        cannon1Anim.SetBool("isShooting", true);
+        cannon2Anim.SetBool("isShooting", true);
+        alumAnim.SetBool("isShooting", true);
+
+
+
         proyectilesLeft--;
 
         if (proyectilesLeft == 0)
         {
-            if(Random.Range(0, 101) < prob)
+            cannon1Anim.SetBool("isShooting", false);
+            cannon2Anim.SetBool("isShooting", false);
+            alumAnim.SetBool("isShooting", false);
+
+            if (Random.Range(0, 101) < prob)
                 bastet.DesiredState(Bastet.States.fists);
             else
                 bastet.DesiredState(Bastet.States.bomb);
