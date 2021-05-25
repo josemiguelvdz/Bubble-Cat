4031113d53a688a16ee7b27debf069b106e4b7f8
josemiguelvdz <josemiguelv2002@gmail.com>
2021-05-25 18:07:45 +0200

    Añadido Canvas de Settings

---
+++ Assets/Scripts/Title/QualityLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QualityLogic : MonoBehaviour
+{
+    public TMP_Dropdown dropdown;
+    public int quality;
+    void Start()
+    {
+        quality = PlayerPrefs.GetInt("qualityNumber", 2);
+        dropdown.value = quality;
+        AdjustQuality();
+    }
+
+    public void AdjustQuality()
+    {
+        QualitySettings.SetQualityLevel(dropdown.value);
+        PlayerPrefs.SetInt("qualityNumber", dropdown.value);
+        quality = dropdown.value;
+    }
+
+}
