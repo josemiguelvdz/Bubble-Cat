31b6adb3ad25a08af4b9b5be9575c9badc50721f
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2021-05-25 11:34:46 +0200

    Menu principal un poco mejor
    Perfeccionado un poco el menu

---
+++ Assets/Scripts/MenuMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMovement : MonoBehaviour
+{
+    float mousePosX, mousePosY;
+    public float movement=20f;
+    RectTransform rT;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rT = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosX = Input.mousePosition.x;
+        mousePosY = Input.mousePosition.y;
+
+        rT.position = new Vector2((mousePosX / Screen.width) * movement + (Screen.width / 2),
+            (mousePosY / Screen.height) * movement + (Screen.height / 2));
+    }
+}
