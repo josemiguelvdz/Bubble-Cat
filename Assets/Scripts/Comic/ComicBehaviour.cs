0b88074b3f54bf768aefa7499316de9db7dc520c
josemiguelvdz <josemiguelv2002@gmail.com>
2021-06-07 00:28:04 +0200

    Juego "final"

---
+++ Assets/Scripts/Comic/ComicBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class ComicBehaviour : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer; // Drag & Drop the GameObject holding the VideoPlayer component
+    public string SceneName;
+    void Start()
+    {
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
